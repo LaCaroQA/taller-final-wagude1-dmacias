4b088ac43784ac4a85d430bed05c97a81ea38b71
wagude1 <wagudelor2019@gmail.com>
2024-08-16 17:42:06 -0500

    creacion menu principal

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,71 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola Winston :)");
+        // construccion menu principal
 
-        System.out.println("Hola Diana Carolina :-)");
+        int menuprincipal;
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("-----------------------------------------------------------");
+        System.out.println("-          MENU DE CAPITULO DE PROGRAMACION               -");
+        System.out.println("-                WAGUDE1 - DMACIAS                        -");
+        System.out.println("-----------------------------------------------------------");
+        System.out.println("- 1. Datos Primitivos                                     -");
+        System.out.println("- 2. String                                               -");
+        System.out.println("- 3. Constantes                                           -");
+        System.out.println("- 4. Tipos de operadores                                  -");
+        System.out.println("- 5. Condicionales IF, ELSE, IF ELSE                      -");
+        System.out.println("- 6. Condicional Switch                                   -");
+        System.out.println("- 7. Condicional Ternaria                                 -");
+        System.out.println("- 8. Bucle DO WHILE                                       -");
+        System.out.println("- 9. Bucle WHILE                                          -");
+        System.out.println("- 10. Bucle FOR                                           -");
+        System.out.println("- 0. Salir                                                -");
+        System.out.println("- por favor digite una opcion -");
+
+        menuprincipal = in.nextInt();
+
+        switch (menuprincipal) {
+            case 1:
+                System.out.println("Has seleccionado Datos Primitivos.");
+
+                break;
+            case 2:
+                System.out.println("Has seleccionado String.");
+                break;
+            case 3:
+                System.out.println("Has seleccionado Constantes.");
+                break;
+            case 4:
+                System.out.println("Has seleccionado Tipos de operadores.");
+                break;
+            case 5:
+                System.out.println("Has seleccionado Condicionales IF, ELSE, IF ELSE.");
+                break;
+            case 6:
+                System.out.println("Has seleccionado Condicional Switch.");
+                break;
+            case 7:
+                System.out.println("Has seleccionado Condicional Ternaria.");
+                break;
+            case 8:
+                System.out.println("Has seleccionado Bucle DO WHILE.");
+                break;
+            case 9:
+                System.out.println("Has seleccionado Bucle WHILE.");
+                break;
+            case 10:
+                System.out.println("Has seleccionado bucle FOR.");
+                break;
+            case 0:
+                System.out.println("Salir del menu...");
+                break;
+            default:
+                System.out.println("Opción no válida. Por favor, intente de nuevo.");
+        }
+        while (menuprincipal == 0);
+        in.close();
     }
 }
